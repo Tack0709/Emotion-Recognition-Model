6c54cc2f26b6d491a0579370732eb12893d95020
takuma <cnj19035@ict.nitech.ac.jp>
2025-11-12 10:21:43 +0900

    modify data division

--- datalodaer.py
+++ datalodaer.py
@@ -6,61 +6,49 @@ import os
 import numpy as np
 
 def get_train_valid_sampler(trainset):
+    """
+    データセットのインデックスサンプラーを返す。
+   
+    """
     size = len(trainset)
     idx = list(range(size))
+    # (注: trainset.session_list のインデックス 0~size-1 をシャッフルする)
     return SubsetRandomSampler(idx)
 
 def load_vocab(dataset_name):
+    """
+    話者語彙とラベル語彙をロード（または定義）する。
+    """
     datapath = os.path.join('../data', dataset_name)
-    speaker_vocab = pickle.load(open(os.path.join(datapath, 'speaker_vocab.pkl'), 'rb'))
     
-    # 5クラス（neu, hap, sad, ang, oth） の語彙を定義
+    # 話者語彙（性別）をロード
+    speaker_vocab_path = os.path.join(datapath, 'speaker_vocab.pkl')
+    if os.path.exists(speaker_vocab_path):
+        speaker_vocab = pickle.load(open(speaker_vocab_path, 'rb'))
+    else:
+        print("Warning: speaker_vocab.pkl not found. Using default {'M': 0, 'F': 1}")
+        speaker_vocab = {'stoi': {'M': 0, 'F': 1}}
+    
+    # ラベル語彙（5クラス）を定義
     label_vocab = {
         'stoi': {'neu': 0, 'hap': 1, 'sad': 2, 'ang': 3, 'oth': 4},
         'itos': {0: 'neu', 1: 'hap', 2: 'sad', 3: 'ang', 4: 'oth'}
     }
-    # (注: 'xxx' は dataset.py で除外されるため、ここには不要)
 
     return speaker_vocab, label_vocab
 
-# collate_fnがNoneを返す可能性に対処するラッパー
-def collate_fn_wrapper(batch):
-    # dataset.py の collate_fn を呼び出す
-    # (data[0] は MultimodalDAGDataset のインスタンス)
-    processed_batch = batch[0][0].collate_fn(batch) 
-    
-    # collate_fn が空のダイアログなどでNoneを返した場合のフィルタリング
-    if processed_batch[0] is None:
-        return None
-    return processed_batch
-
-def filter_none_collate(batch):
-    """
-    DataLoaderに渡すためのcollate関数。
-    dataset.pyのcollate_fnを呼び出し、Noneが返された場合はフィルタリングする。
-    """
-    # バッチ内のデータを使ってcollate_fnを呼び出す (datasetインスタンスが必要)
-    if not batch:
-        return None
-    
-    # バッチの最初の要素から dataset インスタンスを取得
-    # (注: DataLoaderは __getitem__ の戻り値のリストを batch として渡す)
-    # (注2: この実装は DataLoader が dataset インスタンスにアクセスできない前提)
-    # (より良い実装は、 DataLoader 生成時に dataset.collate_fn を直接渡すこと)
-    
-    # この設計では、dataloader.py が dataset.py の実装を知らない前提で
-    # run.py で collate_fn を渡すのが最善。
-    
-    # --> run.py で collate_fn を渡すように変更します (このファイルは変更不要)
-    pass
-
-
 def get_multimodal_loaders(dataset_name = 'IEMOCAP', batch_size=32, num_workers=0, pin_memory=False, args = None):
+    """
+    修正版 MultimodalDAGDataset を使用して、
+    train/valid/test 用の DataLoader を作成する。
+   
+    """
     print('building vocab.. ')
     speaker_vocab, label_vocab = load_vocab(dataset_name)
     
     print('building datasets..')
     # 修正: MultimodalDAGDataset を使用
+    # 'split'引数に基づき、dataset内部で固定分割が行われる
     trainset = MultimodalDAGDataset(dataset_name, 'train',  speaker_vocab, args)
     devset = MultimodalDAGDataset(dataset_name, 'dev', speaker_vocab, args)
     
--- dataset.py
+++ dataset.py
@@ -5,52 +5,84 @@ import pickle, pandas as pd
 import json
 import numpy as np
 import random
-from pandas import DataFrame
-import os # os.path を使用
+import os
 
 class MultimodalDAGDataset(Dataset):
-
-    def __init__(self, dataset_name='IEMOCAP', split='train', speaker_vocab=None, args=None):
+    """
+    固定分割 (Ses1-4: Train/Dev, Ses5: Test) を行うデータセット。
+    .scp ファイル を使用せず、order.pkl のキー（セッションID）に基づいて動的に分割する。
+    """
+    def __init__(self, dataset_name='IEMOCAP', split='train', speaker_vocab=None, args=None, dev_ratio=0.1):
         self.speaker_vocab = speaker_vocab
         self.args = args
-        datapath = os.path.join('../data', dataset_name)
+        datapath = os.path.join('../data', dataset_name) 
 
-        # 1. ERC-SLT22 スタイルで、IDをキーとする辞書を先に読み込む
-        # (注: DAG-ERC の read() とは異なる)
-        
-        # 特徴量 (発話IDがキーの辞書)
+        print(f"Loading {split} data...")
+
+        # 1. IDをキーとする辞書を先に読み込む
         self.bert_features = np.load(os.path.join(datapath, 'bert-base-diag.npy'), allow_pickle=True).item()
         self.w2v2_features = np.load(os.path.join(datapath, 'w2v2-ft-diag.npy'), allow_pickle=True).item()
-        
-        # ラベル (発話IDがキーの辞書)
         self.soft_labels = np.load(os.path.join(datapath, 'IEMOCAP-softlabel-sum.npy'), allow_pickle=True).item()
         self.hard_labels = np.load(os.path.join(datapath, 'IEMOCAP-hardlabel.npy'), allow_pickle=True).item()
 
         # 発話IDの順序 (セッションIDがキーの辞書)
-        with open(os.path.join(datapath, 'order.json'), 'r') as f:
-            self.order = json.load(f)
+        order_path = os.path.join(datapath, 'order.pkl') 
+        with open(order_path, 'rb') as f: 
+            self.order = pickle.load(f) # .pklからロード
             
-        # (新規) 発話テキスト (発話IDがキーの辞書)
-        # (注: この 'text_dict.pkl' は別途作成されている前提)
-        with open(os.path.join(datapath, 'text_dict.pkl'), 'rb') as f:
-            self.text_dict = pickle.load(f)
+        # 発話テキスト (発話IDがキーの辞書)
+        text_dict_path = os.path.join(datapath, 'text_dict.pkl')
+        if os.path.exists(text_dict_path):
+             with open(text_dict_path, 'rb') as f:
+                self.text_dict = pickle.load(f)
+        else:
+            self.text_dict = {}
 
-        # 2. 処理対象のセッションリストを .scp ファイルから読み込む
-        scp_path = os.path.join(datapath, f'iemocap_diag_{split}.scp') # 例: iemocap_diag_train.scp
-        if not os.path.exists(scp_path):
-             # .scp がなければ all (DAG-ERC の split 名に近い)
-             scp_path = os.path.join(datapath, f'iemocap_diag_{split}-all.scp') 
+        # #############################################################
+        # ⬇️ 変更点: .scp 読み込みを削除し、固定分割ロジックを実装
+        # #############################################################
         
-        self.scp_file = pd.read_csv(scp_path, delimiter='\t', header=0).values.tolist()
+        # 2. self.order のキー（セッションID）に基づいてデータを分割
+        all_session_ids = sorted(list(self.order.keys()))
+        train_sessions = []
+        dev_sessions = []
+        test_sessions = []
+
+        for ses_id in all_session_ids:
+            if ses_id.startswith('Ses05'): # Session 5 は Test
+                test_sessions.append(ses_id)
+            elif ses_id.startswith(('Ses01', 'Ses02', 'Ses03', 'Ses04')): # Session 1-4 は Train/Dev
+                train_sessions.append(ses_id)
         
-        self.len = len(self.scp_file)
+        # Session 1-4 から dev_ratio (例: 1割) をランダムに dev に振り分け
+        # (注: 元の ERC-SLT22 は 'cv' (検証) 用に 1/5 を固定で分けていた)
+        random.shuffle(train_sessions)
+        dev_size = int(len(train_sessions) * dev_ratio)
+        dev_sessions = train_sessions[:dev_size]
+        train_sessions = train_sessions[dev_size:]
+
+        if split == 'train':
+            self.session_list = train_sessions
+        elif split == 'dev':
+            self.session_list = dev_sessions
+        elif split == 'test':
+            self.session_list = test_sessions
+        else:
+            raise ValueError(f"Unknown split name: {split}")
+        
+        self.len = len(self.session_list)
+        print(f"Loaded {self.len} dialogues for {split}.")
+        # #############################################################
+
 
     def __getitem__(self, index):
-        # 1. セッションID と範囲を取得 (ERC-SLT22 方式)
-        ses_id, start, end = self.scp_file[index]
+        # 1. セッションIDを取得
+        # (変更点: .scp ではなく、内部リスト self.session_list から取得)
+        ses_id = self.session_list[index]
 
         # 2. 発話IDのリストを取得
-        utt_ids = self.order[ses_id][start:end]
+        # (変更点: start=0, end=max でセッション全体を取得)
+        utt_ids = self.order[ses_id] 
 
         features_text_list = []
         features_audio_list = []
@@ -58,7 +90,7 @@ class MultimodalDAGDataset(Dataset):
         speakers_list = []
         utterances_list = []
 
-        # 3. 発話IDをキーにしてデータを収集
+        # 3. 発話IDをキーにしてデータを収集 (処理は同一)
         for utt_id in utt_ids:
             
             # 3a. 'xxx' (hard_label==5) を除外
@@ -67,27 +99,35 @@ class MultimodalDAGDataset(Dataset):
                 continue
 
             # 3b. 特徴量
-            features_text_list.append(self.bert_features.get(utt_id))
-            features_audio_list.append(self.w2v2_features.get(utt_id))
+            bert_feat = self.bert_features.get(utt_id)
+            w2v2_feat = self.w2v2_features.get(utt_id)
+            
+            if bert_feat is None or w2v2_feat is None:
+                continue
+
+            features_text_list.append(bert_feat)
+            features_audio_list.append(w2v2_feat)
 
             # 3c. ソフトラベル (分布に正規化)
             soft_label_vec = self.soft_labels.get(utt_id)
-            if soft_label_vec is None or hard_label == -1: # ラベル無し
-                labels_soft_list.append(np.array([-1.0] * 5)) # パディング用
+            if soft_label_vec is None or hard_label == -1: 
+                labels_soft_list.append(np.array([-1.0] * 5)) 
             else:
                 label_sum = np.sum(soft_label_vec)
                 if label_sum > 0:
                     labels_soft_list.append(soft_label_vec / label_sum)
-                else:
-                    labels_soft_list.append(np.array([-1.0] * 5)) # パディング用
+                else: 
+                    labels_soft_list.append(np.array([-1.0] * 5)) 
             
             # 3d. 話者ID/性別 (発話ID '..._F000' や '..._M000' から抽出)
-            # (注: これは IEMOCAP の命名規則に依存)
             speaker_char = utt_id.split('_')[-1][0] # 'F' or 'M'
-            speakers_list.append(self.speaker_vocab['stoi'].get(speaker_char, 0)) # 0: default
+            speakers_list.append(self.speaker_vocab['stoi'].get(speaker_char, 0)) 
 
             # 3e. 発話テキスト
             utterances_list.append(self.text_dict.get(utt_id, ""))
+
+        if not speakers_list:
+             return torch.FloatTensor([]), torch.FloatTensor([]), torch.FloatTensor([]), [], 0, []
 
         return torch.FloatTensor(np.array(features_text_list)), \
                torch.FloatTensor(np.array(features_audio_list)), \
@@ -107,9 +147,9 @@ class MultimodalDAGDataset(Dataset):
             a = torch.zeros(max_dialog_len, max_dialog_len)
             for i,s in enumerate(speaker):
                 cnt = 0
-                for j in range(i - 1, -1, -1):             
+                for j in range(i - 1, -1, -1): 
                     a[i,j] = 1
-                    if speaker[j] == s:
+                    if speaker[j] == s: 
                         cnt += 1
                         if cnt==self.args.windowp:
                             break
@@ -134,24 +174,23 @@ class MultimodalDAGDataset(Dataset):
         return torch.stack(s_mask), torch.stack(s_mask_onehot)
 
 
-    def collate_fn(self, data):
+    def collate_fn(self, batch):
         # (DAG-ERC の collate_fn とほぼ同一)
-        # data: (features_text, features_audio, labels_soft, speakers, length, utterances)
-        
-        max_dialog_len = max([d[4] for d in data])
-        if max_dialog_len == 0: # 空のダイアログを除外
+        batch = [d for d in batch if d[4] > 0]
+        if not batch:
             return None, None, None, None, None, None, None, None, None
-        
-        features_text = pad_sequence([d[0] for d in data], batch_first = True) # (B, N, D_t)
-        features_audio = pad_sequence([d[1] for d in data], batch_first = True) # (B, N, D_a)
-        
-        # ソフトラベルのパディング (B, N, C)
-        labels_soft = pad_sequence([d[2] for d in data], batch_first = True, padding_value = -1.0) # (B, N, C)
 
-        adj = self.get_adj_v1([d[3] for d in data], max_dialog_len)
-        s_mask, s_mask_onehot = self.get_s_mask([d[3] for d in data], max_dialog_len)
-        lengths = torch.LongTensor([d[4] for d in data])
-        speakers = pad_sequence([torch.LongTensor(d[3]) for d in data], batch_first = True, padding_value = -1)
-        utterances = [d[5] for d in data]
+        max_dialog_len = max([d[4] for d in batch])
+        
+        features_text = pad_sequence([d[0] for d in batch], batch_first = True) 
+        features_audio = pad_sequence([d[1] for d in batch], batch_first = True) 
+        labels_soft = pad_sequence([d[2] for d in batch], batch_first = True, padding_value = -1.0) 
+
+        adj = self.get_adj_v1([d[3] for d in batch], max_dialog_len)
+        s_mask, s_mask_onehot = self.get_s_mask([d[3] for d in batch], max_dialog_len)
+        
+        lengths = torch.LongTensor([d[4] for d in batch])
+        speakers = pad_sequence([torch.LongTensor(d[3]) for d in batch], batch_first = True, padding_value = -1)
+        utterances = [d[5] for d in batch]
 
         return features_text, features_audio, labels_soft, adj, s_mask, s_mask_onehot, lengths, speakers, utterances
